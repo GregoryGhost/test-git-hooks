dddbe12825ac52951b6664ba684bb89b3f3d9250
Gregory <lobster287@gmail.com>
2020-12-24 13:51:10 +0700

    Add script for apply code style

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TestGitHooks
-
+TEST
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.3.
 
 ## Development server
---
+++ apply-code-style.ps1
@@ -0,0 +1,24 @@
+﻿$gitStatus = Invoke-Expression -Command "git status --porcelain"
+$haveUntrackedChanges = $gitStatus |Where {$_ -match '^\?\?'}
+$haveUncommitedChanges = $gitStatus |Where {$_ -notmatch '^\?\?'}
+if ($haveUntrackedChanges)
+{
+ Write-Error "Есть не отслеживаемые изменения.`
+    Существующие варианты решения:`
+    - затрекать и закоммитить изменения;`
+    - поместить изменения в stash;`
+    - убрать изменения."
+ exit 1
+} elseif ($haveUncommitedChanges) {
+    Write-Error "Есть не закоммиченные изменения.`
+        Существующие варианты решения:`
+        - закоммитить изменения;`
+        - поместить изменения в stash;`
+        - убрать изменения."
+ exit 1
+} else
+{
+    Write-Output "Применяю настройки codestyle'а"
+    $isOkCodeStyle = Invoke-Expression -Command "npx prettier --write . && git commit -am ""[Autocommit] Prettier formatted files"" && npm run build && npm run lint" | Out-Null; $?
+    if ($isOkCodeStyle) { exit 0 } { exit 1}
+}
