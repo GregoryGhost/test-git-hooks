3b454c992087fbe1503ae5b30654db9293fcccf3
Gregory <lobster287@gmail.com>
2020-12-24 18:31:36 +0700

    Fixed miss types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title: number = 'test-git-hooks';
+  title = 'test-git-hooks';
 }
